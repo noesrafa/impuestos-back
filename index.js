6f358d1971e42d7e43956cefa31a1a97d9d74750
Rafael Alexander <noesrafa@gmail.com>
2022-11-05 18:05:48 -0600

    Heroku

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ const server = new ApolloServer({
   },
 });
 
-server.listen().then(({ url }) => {
+server.listen({ port: process.env.PORT || 4000}).then(({ url }) => {
   console.log(`✅ Server ready in port 4000`);
 });
